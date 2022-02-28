4cc5ab5909303a227768f1bbd94c76dc50b1fceb
sr255070 <shagun.rathore@teradata.com>
2022-02-28 23:00:08 +0530

    My commit

--- SpringMicroservices/SpringCloudConfigServer/src/main/java/com/spring/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ SpringMicroservices/SpringCloudConfigServer/src/main/java/com/spring/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,10 +2,10 @@ package com.spring.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@Configuration
+@EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
     public static void main(String[] args) {
